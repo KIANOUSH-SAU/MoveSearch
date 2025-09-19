8c9be6660faa646f9d9f0582e579541ed1131868
NOTNETKIA <seddighpour.kianoush@gmail.com>
2025-09-19 10:14:12 +0300

    - New packages for routing were added - Routing logic was implemented - Favorites page was added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 function App() {
 	return (
 		<>
-			<MovieCard
-				movie={{
-					title: "First movie",
-					release_date: "2024",
-				}}
-			/>
+			<main className="main-content">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/favorites" element={<Favorites />} />
+				</Routes>
+			</main>
 		</>
 	);
 }
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,8 @@
+export default function Favorites() {
+	return (
+		<div className="favorites-empty">
+			<h2>No favorite movies yet</h2>
+			<p>Start adding movies to favorites...</p>
+		</div>
+	);
+}
