b2b7549ce536a1f4a16ffbb55cba7630844f8d29
NOTNETKIA <seddighpour.kianoush@gmail.com>
2025-09-19 16:53:22 +0300

    - Fetching data from the api to the MovieCard component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
 	const [searchQuery, setSearchQuery] = useState("");
 	const [movies, setMovies] = useState([]);
 	const [error, setError] = useState(null);
-	const [loading, setloading] = useState(true);
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const loadPopularMovies = async () => {
@@ -17,16 +17,28 @@ export default function Home() {
 				setError("Failed to load movies...");
 				console.log(err);
 			} finally {
-				setloading(false);
+				setLoading(false);
 			}
 		};
 		loadPopularMovies();
 	}, []);
 
-	const handleSearch = (e) => {
+	const handleSearch = async (e) => {
 		e.preventDefault();
-		alert(searchQuery);
-		setSearchQuery("");
+		if (!searchQuery.trim()) return;
+		if (loading) return;
+		setLoading(true);
+
+		try {
+			const searchResults = await searchMovies(searchQuery);
+			setMovies(searchResults);
+			setError(null);
+		} catch (err) {
+			console.log(err);
+			setError("Failed to search for movies...");
+		} finally {
+			setLoading(false);
+		} //setSearchQuery("");
 	};
 	return (
 		<>
@@ -43,11 +55,16 @@ export default function Home() {
 						Search
 					</button>
 				</form>
-				<div className="movies-grid">
-					{movies.map((movie) => (
-						<MovieCard movie={movie} key={movie.id} />
-					))}
-				</div>
+				{error && <div className="error-message">{error}</div>}
+				{loading ? (
+					<div className="loading">Loading...</div>
+				) : (
+					<div className="movies-grid">
+						{movies.map((movie) => (
+							<MovieCard movie={movie} key={movie.id} />
+						))}
+					</div>
+				)}
 			</div>
 		</>
 	);
